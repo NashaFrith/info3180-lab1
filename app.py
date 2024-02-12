2b9a9dc3e54ad10182e3cd9c8fb3458488ddbd7a
NashaFrith <44258016+NashaFrith@users.noreply.github.com>
2024-02-11 21:45:11 -0500

    Create my first template file

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ app = Flask(__name__)
 def home():
     return 'My home page'
 
+@app.route ('/about')
+def about():
+    """About Page"""
+    return render_template('about.html')
 
 @app.errorhandler(404)
 def page_not_found(error):
